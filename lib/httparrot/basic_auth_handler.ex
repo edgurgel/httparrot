2e08a8b6fd687653925dad61d596421252c21012
Eduardo Gurgel <edgurgel@gmail.com>
2013-12-31 05:44:14 -0300

    Add '/basic-auth/:user/:passwd'

---
+++ lib/httparrot/basic_auth_handler.ex
@@ -0,0 +1,35 @@
+defmodule HTTParrot.BasicAuthHandler do
+  alias HTTParrot.GeneralRequestInfo
+
+  def init(_transport, _req, _opts) do
+    {:upgrade, :protocol, :cowboy_rest}
+  end
+
+  def allowed_methods(req, state) do
+    {["GET"], req, state}
+  end
+
+  def is_authorized(req, state) do
+    {user, req} = :cowboy_req.binding(:user, req)
+    {passwd, req} = :cowboy_req.binding(:passwd, req)
+    {:ok, auth, req} = :cowboy_req.parse_header("authorization", req)
+    case auth do
+      {"basic", {^user, ^passwd}} -> {true, req, user}
+      _ -> {{false, "Basic realm=\"Fake Realm\""}, req, state}
+    end
+  end
+
+  def content_types_provided(req, state) do
+    {[{{"application", "json", []}, :get_json}], req, state}
+  end
+
+  def get_json(req, user) do
+    {response(user), req, nil}
+  end
+
+  defp response(user) do
+    [authenticated: true, user: user] |> JSEX.encode!
+  end
+
+  def terminate(_, _, _), do: :ok
+end
