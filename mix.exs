c92ce8e54da1ce4b64155ce8b8ccc98ac8f45838
Eduardo Gurgel <eduardo@gurgel.me>
2014-09-03 21:41:27 +1200

    Bump version 0.3.3
    [ci skip]

--- mix.exs
+++ mix.exs
@@ -7,7 +7,7 @@ defmodule Httparrot.Mixfile do
 
   def project do
     [ app: :httparrot,
-      version: "0.3.2",
+      version: "0.3.3",
       elixir: "~> 0.14.1 or ~> 0.15.0 or ~> 1.0.0-rc1",
       name: "HTTParrot",
       description: @description,
