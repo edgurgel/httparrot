5b3409d5b17f1772a1e503ce9da6b7b8390c0537
qgadrian <qgadrian@users.noreply.github.com>
2020-06-29 13:33:39 +0200

    Bump deps

--- mix.exs
+++ mix.exs
@@ -26,7 +26,7 @@ defmodule Httparrot.Mixfile do
 
   defp deps do
     [
-      {:cowboy, "~> 2.6.3"},
+      {:cowboy, "~> 2.8.0"},
       {:exjsx, "~> 3.0 or ~> 4.0"},
       {:con_cache, "~> 0.14.0"},
       {:ex_doc, "~> 0.14", only: :dev},
